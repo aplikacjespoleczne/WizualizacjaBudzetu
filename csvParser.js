020c9357e6200862edbf2861eb80c9d6ec872794
filipabramowicz <filip.abramowicz@gmail.com>
2014-07-28 23:27:39 +0200

    [fix] correct handling of asynchronous tasks added

--- csvParser.js
+++ csvParser.js
@@ -1,7 +1,8 @@
 var Converter = require('csvtojson').core.Converter;
 var fs = require('fs');
+var async = require("async");
 
-exports.parseCSVFile = function(filepath) {
+module.exports = function(filepath) {
 
   var fileStream = fs.createReadStream(filepath);
   var param = {
@@ -13,66 +14,89 @@ exports.parseCSVFile = function(filepath) {
   fileStream.pipe(csvConverter);
 
   csvConverter.on("end_parsed", function(jsonObj) {
+    
     var key;
-
+    
     var MongoClient = require('mongodb').MongoClient, format = require('util').format;
     MongoClient.connect('mongodb://mo14248_wiz_budz:kdpWizualizujeBudzet123@mongo0.mydevil.net:27017/mo14248_wiz_budz', function(err, db) {
-         
-      var collection = db.collection("main");
-      collection.remove({}, function(err, removed) {
-        collection.insert(jsonObj, function(err, result) {
-          if (err) throw err;
-          db.close();
-        });
-      });
-      
-      for (var element in jsonObj) {
-        console.log(jsonObj[element]);
-        key = parseInt(jsonObj[element]['Dzial - numer']);
+     
+      async.series([
+        function(callback){
+          process.stderr.write("STEP 1\n");
+          collection = db.collection("main");
+          collection.remove({}, function(err, removed) {
+            if (err) throw err;
+            collection.insert(jsonObj, function(){
+              callback(null, 1);
+            });
+          });        
+        },
+        function(callback) {
+          
+          process.stderr.write("STEP 2\n");
+
+          for (var element in jsonObj) {
+            //console.log(jsonObj[element]);
+            key = parseInt(jsonObj[element]['Dzial - numer']);
        
-        if (level1_hash.hasOwnProperty(key)) {
-          level1_hash[key] = level1_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(' ',''));
-        } else {
-          level1_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(' ',''));
-        }    
-      }
+            if (level1_hash.hasOwnProperty(key)) {
+              level1_hash[key] = level1_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(' ',''));
+            } else {
+              level1_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(' ',''));
+            }    
+          }
 
-      collection = db.collection("null:null");
-      collection.insert(level1_hash, function(err, result) {
-        if (err) throw err;
-        db.close();
+          collection = db.collection("null:null");
+          collection.remove({}, function(err, removed) {
+            if (err) throw err;
+            collection.insert(level1_hash, function(err, result) {
+              if (err) throw err;
+              process.stderr.write("STEP 3\n");  
+              callback(null, 2);
+            }); 
+          });
+        },
+        function(callback) {
+          for (main_key in level1_hash) {
+            if (main_key != "_id" ) {
+              var level2_hash = {};
+              for (var element in jsonObj) {
+                if (parseInt(jsonObj[element]['Dzial - numer']) == main_key){
+                  key = parseInt(jsonObj[element]['Rozdzial - numer']);
+                  if (level2_hash.hasOwnProperty(main_key)) {
+                    level2_hash[key] = level2_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(' ',''));
+                  } else {
+                    level2_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(' ',''));
+                  }
+                }
+              }
+              name = main_key + ":null";
+              //console.log(name);
+              collection = db.collection(name);
+              collection.insert(level2_hash, function(err, result) {
+                if (err) throw err;
+              });         
+            }
+          }
+          callback(null, 3);
+          console.log("DONE");
+        }
+      ], function(error, results) {
+        console.log(results); 
       });  
-
-    /*
+      
+      /*
       console.log(level1_hash); 
       for (key in level1_hash) {
         get_level2(jsonObj, key);
       }
-    */
-      for (main_key in level1_hash) {
-        for (var element in jsonObj) {
-          if (parseInt(jsonObj[element]['Dzial - numer']) == main_key){
-            key = parseInt(main_hash[element]['Rozdzial - numer']);
-            if (level2_hash.hasOwnProperty(key)) {
-              level2_hash[key] = level2_hash[key] + parseInt((main_hash[element]['Kwota [PLN]']).replace(' ',''));
-            } else {
-              level2_hash[key] = parseInt((main_hash[element]['Kwota [PLN]']).replace(' ',''));
-            }
-          }
-        }
-        name = key + ":null";
-        collection = db.collection(name);
-        collection.insert(level1_hash, function(err, result) {
-          if (err) throw err;
-          db.close();
-        }); 
-      }
-      db.close();
-    });
+      */
+    }); 
   });
 }
 
-/* Seems not needed but let's wait a while to see 
+/*  
+Seems not needed but let's wait a while to see 
 function get_level3(main_hash, lev1_num, lev2_num) {
 
   var level3_hash = {};
