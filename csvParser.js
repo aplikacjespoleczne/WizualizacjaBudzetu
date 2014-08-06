73c7d9c6af337cd081459ed4202ef7a3a3625efd
filipabramowicz <filip.abramowicz@gmail.com>
2014-08-06 23:38:44 +0200

    [opt] csvParser refactoring & N-element docs inserted into DB

--- csvParser.js
+++ csvParser.js
@@ -16,25 +16,6 @@ var parse = function(filepath) {
 
   fileStream.pipe(csvConverter);
 
-  var async_level3 = function(jsonObj, main_key, sec_key, callback) {
-    process.nextTick(function(){
-      var level3_hash = {};
-      for (var element in jsonObj) {
-        if (parseInt(jsonObj[element]['Dział - numer']) == main_key){
-          if (parseInt(jsonObj[element]['Rozdział - numer']) == sec_key) {
-            key = parseInt(jsonObj[element]['Zadanie - numer']);
-            if (level3_hash.hasOwnProperty(key)) {
-              console.log("Real shit!!!!!!!!!!!!!!!!!!!!!")
-            } else {
-              level3_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
-            }                      
-          }
-        }
-      }
-      callback(level3_hash, main_key, sec_key);
-    });
-  };
-
   csvConverter.on("end_parsed", function(jsonObj) {
     
     var key;
@@ -45,89 +26,91 @@ var parse = function(filepath) {
         function(callback){
           process.stderr.write("STEP 1\n");
           collection = db.collection("main");
-          collection.remove({}, function(err, removed) {
-            if (err) throw err;
-            collection.insert(jsonObj, function(){
-              callback(null, 1);
-            });
+          collection.insert(jsonObj, function(){
+            callback(null, 1);
           });        
         },
-        function(callback) {
-          
+        function(callback){
           process.stderr.write("STEP 2\n");
-
-          for (var element in jsonObj) {
-            //console.log(jsonObj[element]);
-            key = parseInt(jsonObj[element]['Dział - numer']);
-            if (level1_hash.hasOwnProperty(key)) {
-              level1_hash[key] = level1_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
-            } else {
-              level1_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
-            }    
-          }
-          console.log(level1_hash);
-          collection = db.collection("null:null");
-          collection.remove({}, function(err, removed) {
-            if (err) throw err;
-            collection.insert(level1_hash, function(err, result) {
-              if (err) throw err;
-              process.stderr.write("STEP 3\n");  
-              callback(null, 2);
-            }); 
+          createStructureLevel1(jsonObj, function(result_hash){
+            level1_hash = result_hash;
+            injectDatabase(db, "null:null", level1_hash);
+            callback(null, 2);            
           });
         },
         function(callback) {
           for (main_key in level1_hash) {
-            var level2_hash = {};
             if (main_key != "_id" ) {
-              for (var element in jsonObj) {
-                if (parseInt(jsonObj[element]['Dział - numer']) == main_key){
-                  key = parseInt(jsonObj[element]['Rozdział - numer']);
-                  if (level2_hash.hasOwnProperty(key)) {
-                    level2_hash[key] = level2_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
-                  } else {
-                    level2_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
+              key1 = main_key;
+              process.nextTick(createStructureLevel2(jsonObj, key1, function(result_hash, key1){
+                name = key1 + ":null";
+                injectDatabase(db, name, result_hash);               
+                level2_hash = result_hash;
+                for (var l2_key in level2_hash){
+                  if (l2_key != "_id" ){ 
+                    key2 = l2_key;
+                    process.nextTick(createStructureLevel3(jsonObj, key1, key2, function(result_hash, key1, key2){
+                      name = key1 + ":" + key2;
+                      injectDatabase(db, name, result_hash);               
+                    }));
                   }
-                }
-              }
-              name = main_key + ":null";
-              console.log(main_key + " ----------------------");
-              console.log(level2_hash);
-              collection = db.collection(name);
-              collection.insert(level2_hash, function(err, result) {
-                if (err) throw err;
-              });
-              for (var l2_key in level2_hash){
-                if (l2_key != "_id" ) {                
-                  async_level3(jsonObj, main_key, l2_key, function(level3_hash, main_key, l2_key){
-                    
-                    name = main_key + ":" + l2_key;
-                    console.log(main_key + " - " + l2_key);
-                    console.log(level3_hash);
-                    collection = db.collection(name);
-                    collection.insert(level3_hash, function(err, result) {
-                      if (err) throw err;
-                    }); 
-                  });
-                }
-              }                     
+                }                  
+              }));
             }
           }
-          callback(null, 3);
+          callback(null, 3); 
           console.log("DONE!");
         }
       ], function(error, results) {
         console.log(results); 
       });  
-      
-      /*
-      console.log(level1_hash); 
-      for (key in level1_hash) {
-        get_level2(jsonObj, key);
-      }
-      */
     }); 
   });
+}
+
+var createStructureLevel1 = function(jsonObj, callback) {
+  var result_hash = {};
+  for (var element in jsonObj) {
+    key = parseInt(jsonObj[element]['Dział - numer']);
+    if (result_hash.hasOwnProperty(key)) {
+      result_hash[key] = result_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
+    } else {
+      result_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
+    }
+  }
+  callback(result_hash);
+}
+
+var createStructureLevel2 = function(jsonObj, main_key, callback){
+  return function() {
+    var result_hash = {};
+    for (var element in jsonObj) {
+      if (parseInt(jsonObj[element]['Dział - numer']) == main_key){
+        key = parseInt(jsonObj[element]['Rozdział - numer']);
+        if (result_hash.hasOwnProperty(key)) {
+          result_hash[key] = result_hash[key] + parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
+        } else {
+          result_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
+        }
+      }
+    }
+    callback(result_hash, main_key);
+  }
+}
+
+var createStructureLevel3 = function(jsonObj, main_key, sec_key, callback) {
+  return function() {
+    var result_hash = {};
+    for (var element in jsonObj) {
+      if (parseInt(jsonObj[element]['Dział - numer']) == main_key){
+        if (parseInt(jsonObj[element]['Rozdział - numer']) == sec_key) {
+          key = parseInt(jsonObj[element]['Zadanie - numer']);
+          result_hash[key] = parseInt((jsonObj[element]['Kwota [PLN]']).replace(/\s+/g,''));
+        }
+      }
+    }
+    callback(result_hash, main_key, sec_key);
+  }
 }
 
 var cleanDB = function() {
@@ -139,6 +122,17 @@ var cleanDB = function() {
       });     
     });
   });
+}
+
+var injectDatabase = function(db, name, hash) {
+  collection = db.collection(name);
+  for (var key in hash) {
+    var row = {};
+    row[key] = hash[key];
+    collection.insert(row, function(err, result) {
+      if (err) throw err;
+    });
+  }
 }
 
 exports.parse = parse;
