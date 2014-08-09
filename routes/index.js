55d9324b6d7fe7441a8eb35fa264573574fde1b5
jmachlan <j.machlanski@gmail.com>
2014-08-09 16:27:25 +0200

    first use of d3, diagram with simple data visualisation added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var parseCSVFile = require('../csvParser');
 var AmountProvider = require('../amountProvider').AmountProvider;
 var amountProvider = new AmountProvider();
+var d3 = require('d3');
+var fs = require('fs');
+var jsdom = require('jsdom');
 
 /* GET home page. */
 router.get('/', function(req, res){
@@ -66,6 +69,54 @@ router.get('/get/:first/:second', function(req, res) {
 		}
 		res.json(results);	
 	});
+});
+
+///////////////////////d3
+router.get('/chart', function(req, res) {
+	amountProvider.findTest(function(error, results) {
+		if (error) {
+			throw error;
+		}
+		res.writeHead(200, {'Content-Type': 'application/json'});	
+		res.json(results);	
+	});
+	
+});
+router.get('/chart-test', function(req, res) {
+	var htmlStructure = 
+		'<html>'+
+			'<head></head>'+
+			'<body>'+
+				'<div id="main-container"></div>'+
+				'<script src="http://d3js.org/d3.v3.min.js"></script>'+
+			'</body>'+
+		'</html>';
+	
+	jsdom.env({ features : { QuerySelector : true }, html : htmlStructure, done : function(errors, window) {
+		if (errors) {
+			console.log(errors);
+		}
+		var el = window.document.querySelector('#main-container');
+		var body = window.document.querySelector('body');
+		
+		amountProvider.findTest(function(error, results) {
+			var canvas = d3.select(el)
+							.append('svg:svg')
+								.attr('width', 1000)
+								.attr('height', 1000);
+		
+			var bars = canvas.selectAll("rect")
+						.data(results)
+						.enter()
+							.append("rect")
+								.attr("width", 50)
+								.attr("height", function(d) { return d.value * 5;})
+								.attr("x", function(d, i) { return i * 60;})
+								.attr("y", function(d, i) { return 500  - d.value * 5;});
+
+			res.send(window.document.innerHTML);					
+		});
+	}});
 });
 
 module.exports = router;
